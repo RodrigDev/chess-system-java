efbe7ef1744d1a87924d05a2f756f3523d74427d
Rodrigo de Oliveira <rodrigdeveloper@gmail.com>
2020-06-02 17:18:02 -0300

    moving pieces

--- src/boardgame/BoardException.java
+++ src/boardgame/BoardException.java
@@ -1,11 +1,11 @@
 package boardgame;
 
-public class BoardExeption extends RuntimeException {
+public class BoardException extends RuntimeException {
 
 
 	private static final long serialVersionUID = 1L;
 	
-	public BoardExeption(String msg) {
+	public BoardException(String msg) {
 		super(msg);
 	}
 }
